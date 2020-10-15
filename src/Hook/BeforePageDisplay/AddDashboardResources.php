b26b73562904fd471273daab24797cc8ac258f90
pwirth <wirth@hallowelt.biz>
2020-10-15 17:27:57 +0200

    Fix missing label in user and admin dashboard
    ERM:21302
    [3.1.x]
    
    Change-Id: Iaca6deda2da6749c667a7685a3d71784c1969d54

---
+++ src/Hook/BeforePageDisplay/AddDashboardResources.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace BlueSpice\RSSFeeder\Hook\BeforePageDisplay;
+
+use BlueSpice\Hook\BeforePageDisplay;
+use SpecialPage;
+
+class AddDashboardResources extends BeforePageDisplay {
+
+	/**
+	 *
+	 * @var array
+	 */
+	protected $pages = [
+		"AdminDashboard",
+		"UserDashboard",
+	];
+
+	/**
+	 *
+	 * @return bool
+	 */
+	protected function skipProcessing() {
+		$title = $this->out->getTitle();
+
+		foreach ( $this->pages as $spPage ) {
+			if ( !$this->getServices()->getSpecialPageFactory()->exists( $spPage ) ) {
+				continue;
+			}
+			if ( !$title->equals( SpecialPage::getTitleFor( $spPage ) ) ) {
+				continue;
+			}
+			return false;
+		}
+		return true;
+	}
+
+	protected function doProcess() {
+		$this->out->addModules( 'ext.bluespice.rssFeeder' );
+	}
+
+}
