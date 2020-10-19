51fb3e5f2a79e06db4640946097e8d13e510b06e
Dejan Savuljesku <d.savuljesku@gmail.com>
2020-10-19 16:21:09 +0200

    Refactor and fix security holes
    [3.1.x]
    
    ERM 21304
    
    Change-Id: If4686e37187708037f852e1c78feed70b908390d

--- resources/bluespice.rssStandards.js
+++ resources/bluespice.rssStandards.js
@@ -3,50 +3,41 @@ Ext.onReady( function() {
 		'BS.RSSStandards',
 		mw.config.get('wgScriptPath') + '/extensions/BlueSpiceRSSFeeder/resources/BS.RSSStandards'
 	);
+	bs.util.registerNamespace( 'bs.rssfeeder.handler' );
 
-	var combo;
-
-	// TODO SW: make generic
-	var buttons = {
-		rc: Ext.get('btnFeedRc'),
-		own: Ext.get('btnFeedOwn'),
-		page: Ext.get('btnFeedPage'),
-		ns: Ext.get('btnFeedNs'),
-		cat: Ext.get('btnFeedCat'),
-		watch: Ext.get('btnFeedWatch')
+	bs.rssfeeder.handler = {
+		namespace: function() {
+			location.href = Ext.get( 'selFeedNs' ).dom.value;
+		},
+		watchlist: function() {
+			location.href = Ext.get('selFeedWatch').dom.value;
+		},
+		category: function() {
+			location.href = Ext.get('selFeedCat').dom.value;
+		},
+		page: function() {
+			if ( !combo.getValue() ) {
+				return;
+			}
+			var link = combo.getValue().data.feedUrl;
+			if ( link ) {
+				location.href = link;
+			}
+		}
 	};
 
-	buttons.rc.addListener('click', function() {
-		location.href = this.dom.value;
-	});
+	var buttons = $( '#RSSFeederForm' ).find( 'button' ),
+		callbacks = mw.config.get( 'bsRSSFeederFeedCallbacks' ),
+		combo = Ext.create( 'BS.RSSStandards.form.field.TitleWithUrlCombo', {
+			renderTo: 'divFeedPage'
+		} );
 
-	buttons.own.addListener('click', function() {
-		location.href = this.dom.value;
-	});
-
-	buttons.ns.addListener('click', function() {
-		location.href = Ext.get('selFeedNs').dom.value;
-	});
-
-	buttons.cat.addListener('click', function() {
-		location.href = Ext.get('selFeedCat').dom.value;
-	});
-
-	buttons.watch.addListener('click', function() {
-		location.href = Ext.get('selFeedWatch').dom.value;
-	});
-
-	combo = Ext.create( 'BS.RSSStandards.form.field.TitleWithUrlCombo', {
-		renderTo: 'divFeedPage'
-	});
-
-	buttons.page.addListener( 'click', function(){
-		if ( !combo.getValue() ) {
+	buttons.on( 'click', function( e ) {
+		var id = $( this ).attr( 'id');
+		if ( callbacks.hasOwnProperty( id ) ) {
+			bs.util.runCallback( callbacks[id] );
 			return;
 		}
-		var link = combo.getValue().data.feedUrl;
-		if ( link ) {
-			location.href = link;
-		}
-	});
-});
+		location.href = $( this ).val();
+	} );
+} );
---
+++ src/RSSFeed/FeedBase.php
@@ -0,0 +1,146 @@
+<?php
+
+namespace BlueSpice\RSSFeeder\RSSFeed;
+
+use BlueSpice\RSSFeeder\IRSSFeed;
+use ConfigException;
+use IContextSource;
+use MediaWiki\MediaWikiServices;
+use MWException;
+use RSSCreator;
+use SpecialPage;
+use User;
+use ViewFormElementButton;
+use ViewFormElementFieldset;
+use ViewFormElementLabel;
+
+abstract class FeedBase implements IRSSFeed {
+	/** @var IContextSource */
+	protected $context;
+	/** @var User */
+	protected $user;
+	/** @var MediaWikiServices */
+	protected $services;
+
+	/**
+	 * @param IContextSource $context
+	 * @param User $user
+	 * @param MediaWikiServices $services
+	 */
+	public function __construct(
+		IContextSource $context, User $user, MediaWikiServices $services
+	) {
+		$this->context = $context;
+		$this->user = $user;
+		$this->services = $services;
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public static function factory(
+		IContextSource $context, User $user, MediaWikiServices $services
+	) {
+		return new static( $context, $user, $services );
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getViewElement() {
+		$set = $this->getViewElementFieldset();
+		$set->addItem( $this->getLabelElement() );
+		$set->addItem( $this->getSubmitButton() );
+
+		return $set;
+	}
+
+	/**
+	 * @return ViewFormElementFieldset
+	 */
+	protected function getViewElementFieldset() {
+		$set = new ViewFormElementFieldset();
+		$set->setLabel( $this->getDisplayName()->plain() );
+
+		return $set;
+	}
+
+	/**
+	 * @return ViewFormElementButton
+	 * @throws MWException
+	 */
+	protected function getSubmitButton() {
+		$btn = new ViewFormElementButton();
+		$btn->setId( $this->getButtonId() );
+		$btn->setName( $this->getButtonId() );
+		$btn->setType( 'button' );
+		$btn->setValue( $this->getFeedURL() );
+		$btn->setLabel( $this->context->msg( 'bs-rssfeeder-submit' )->plain() );
+
+		return $btn;
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	protected function getFeedURL( $params = [] ) {
+		return SpecialPage::getTitleFor( 'RSSFeeder' )->getLocalUrl(
+			array_merge( [
+				'Page' => $this->getId(),
+			], $this->getUserAuthInfo(), $params )
+		);
+	}
+
+	/**
+	 * @return ViewFormElementLabel
+	 */
+	protected function getLabelElement() {
+		$label = new ViewFormElementLabel();
+		$label->useAutoWidth();
+		$label->setFor( $this->getButtonId() );
+		$label->setText( $this->getDescription()->plain() );
+
+		return $label;
+	}
+
+	/**
+	 * @param string|null $displayName
+	 * @return false|RSSCreator
+	 * @throws ConfigException
+	 */
+	protected function getChannel( $displayName = null ) {
+		if ( !$displayName ) {
+			$displayName = $this->getDisplayName()->plain();
+		}
+		$sitename = $this->services->getMainConfig()->get( 'Sitename' );
+		return RSSCreator::createChannel(
+			RSSCreator::xmlEncode( $sitename . ' - ' . $displayName ),
+			'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'],
+			$this->getDescription()->plain()
+		);
+	}
+
+	/**
+	 * @return string
+	 */
+	protected function getButtonId() {
+		return $this->getId();
+	}
+
+	/**
+	 * @return array
+	 */
+	protected function getUserAuthInfo() {
+		return [
+			'u' => $this->user->getName(),
+			'h' => $this->user->getToken(),
+		];
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getJSHandler() {
+		return '';
+	}
+}
