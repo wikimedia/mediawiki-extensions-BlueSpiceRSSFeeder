0c9223cbbc0dc6a8ab3b0d27750fc60c8e11ac9a
Dejan Savuljesku <d.savuljesku@gmail.com>
2020-10-30 11:30:38 +0100

    Add configuration for UrlWhitelist and max number of redirects
    [3.1.x]
    
    ERM 21304
    
    Change-Id: Icbfd9d07079f9837f069e7d6c2d5bf5c74d29dfe

---
+++ src/ConfigDefinition/MaxRedirects.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace BlueSpice\RSSFeeder\ConfigDefinition;
+
+use BlueSpice\ConfigDefinition\IntSetting;
+use BlueSpice\ConfigDefinition\IOverwriteGlobal;
+
+class MaxRedirects extends IntSetting implements IOverwriteGlobal {
+
+	/**
+	 *
+	 * @return string[]
+	 */
+	public function getPaths() {
+		return [
+			static::MAIN_PATH_FEATURE . '/' . static::FEATURE_ADMINISTRATION . '/BlueSpiceRSSFeeder',
+			static::MAIN_PATH_EXTENSION . '/BlueSpiceRSSFeeder/' . static::FEATURE_ADMINISTRATION ,
+			static::MAIN_PATH_PACKAGE . '/' . static::PACKAGE_FREE . '/BlueSpiceRSSFeeder',
+		];
+	}
+
+	/**
+	 *
+	 * @return string
+	 */
+	public function getLabelMessageKey() {
+		return 'bs-rss-feeder-pref-max-redirects';
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getGlobalName() {
+		return 'wgRSSUrlNumberOfAllowedRedirects';
+	}
+}
