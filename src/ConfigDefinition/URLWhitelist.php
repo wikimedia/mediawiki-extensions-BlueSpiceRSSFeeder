05e81b679a14ba252524c6be27d9e73e8919cc13
Dejan Savuljesku <d.savuljesku@gmail.com>
2020-10-30 11:30:38 +0100

    Add configuration for UrlWhitelist and max number of redirects
    [3.1.x]
    
    ERM 21304
    
    Change-Id: Icbfd9d07079f9837f069e7d6c2d5bf5c74d29dfe

---
+++ src/ConfigDefinition/URLWhitelist.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace BlueSpice\RSSFeeder\ConfigDefinition;
+
+use BlueSpice\ConfigDefinition\ArraySetting;
+use BlueSpice\ConfigDefinition\IOverwriteGlobal;
+use HTMLMultiSelectPlusAdd;
+
+class URLWhitelist extends ArraySetting implements IOverwriteGlobal {
+
+	/**
+	 *
+	 * @return string[]
+	 */
+	public function getPaths() {
+		return [
+			static::MAIN_PATH_FEATURE . '/' . static::FEATURE_ADMINISTRATION . '/BlueSpiceRSSFeeder',
+			static::MAIN_PATH_EXTENSION . '/BlueSpiceRSSFeeder/' . static::FEATURE_ADMINISTRATION ,
+			static::MAIN_PATH_PACKAGE . '/' . static::PACKAGE_FREE . '/BlueSpiceRSSFeeder',
+		];
+	}
+
+	/**
+	 *
+	 * @return string
+	 */
+	public function getLabelMessageKey() {
+		return 'bs-rss-feeder-pref-url-whitelist';
+	}
+
+	/**
+	 *
+	 * @return HTMLMultiSelectPlusAdd
+	 */
+	public function getHtmlFormField() {
+		return new HTMLMultiSelectPlusAdd( $this->makeFormFieldParams() );
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getGlobalName() {
+		return 'wgRSSUrlWhitelist';
+	}
+}
